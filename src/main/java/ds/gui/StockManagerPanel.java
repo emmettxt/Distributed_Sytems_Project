9b260fa35385fcaf010bc8da2e8c985fabedffd8
Emmet Moore <x21192090@student.ncirl.ie>
2022-08-01 23:10:43 +0100

    AddStock implemented in GUI

--- src/main/java/ds/gui/StockManagerPanel.java
+++ src/main/java/ds/gui/StockManagerPanel.java
@@ -1,57 +1,148 @@
 package ds.gui;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Iterator;
 
+import javax.sound.midi.SysexMessage;
 import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 import ds.stockManager.EmptyStockMessage;
 import ds.stockManager.StockDatabase;
 import ds.stockManager.StockManagerClient;
 import ds.stockManager.StockMessage;
+import io.grpc.stub.StreamObserver;
 
-class StockManagerPanel{
-	public JPanel servicePanel;
-	public String Label = "Stock Manager";
-	// TODO Enable AnimalLocatorClient
-	private StockManagerClient client = new StockManagerClient();
+class StockManagerPanel {
+  public JPanel servicePanel;
+  public String Label = "Stock Manager";
+  // TODO Enable AnimalLocatorClient
+  private StockManagerClient client = new StockManagerClient();
 
-	StockManagerPanel(){
+  StockManagerPanel() {
     servicePanel = new JPanel();
-		servicePanel.setVisible(false);
-		servicePanel.setLayout(new BoxLayout(servicePanel, BoxLayout.Y_AXIS));
+    servicePanel.setVisible(false);
+    servicePanel.setLayout(new BoxLayout(servicePanel, BoxLayout.Y_AXIS));
     refreshCurrentStockTypes();
     servicePanel.add(addStockPanel());
-	}
+  }
+
   private ArrayList<String> currentStockTypes;
   private ArrayList<StockMessage> currentStock;
 
-  private void refreshCurrentStockTypes(){
-    EmptyStockMessage emptyStockMessage  = EmptyStockMessage.newBuilder().build();
+  private void refreshCurrentStockTypes() {
+    EmptyStockMessage emptyStockMessage = EmptyStockMessage.newBuilder().build();
     Iterator<StockMessage> stocksummary = client.getBlockingStub().stockSummary(emptyStockMessage);
     currentStockTypes = new ArrayList<String>();
     currentStock = new ArrayList<StockMessage>();
-    while(stocksummary.hasNext()){
+    while (stocksummary.hasNext()) {
       StockMessage s = stocksummary.next();
       currentStockTypes.add(s.getStockType());
       currentStock.add(s);
     }
-    
+
   }
-  private JPanel addStockPanel(){
+
+  StreamObserver<StockMessage> addStockStreamObserver;
+
+  private JPanel addStockPanel() {
     JPanel addStockPanel = new JPanel();
+
     String[] arr = new String[currentStockTypes.size()];
     arr = currentStockTypes.toArray(arr);
     JComboBox<String> stockCJComboBox = new JComboBox<String>(arr);
-
     stockCJComboBox.setEditable(true);
 
+    JButton newStreamButton = new JButton("New Add Stock Stream");
+
+    JTextField stockVolumeTextField = new JTextField(2);
+
+    JButton addButton = new JButton("Add Stock");
+
+    // add action listener to new stream button which is also close stream button
+    newStreamButton.addActionListener(new ActionListener() {
+
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        // toggle buttons
+        stockCJComboBox.setEnabled(!stockCJComboBox.isEnabled());
+        stockVolumeTextField.setEnabled(!stockVolumeTextField.isEnabled());
+        addButton.setEnabled(!addButton.isEnabled());
+
+        // if the button is in new stream mode
+        if (newStreamButton.getText().equals("New Add Stock Stream")) {
+          newStreamButton.setEnabled(!newStreamButton.isEnabled());
+          newStreamButton.setText("Close Stream");
+          System.out.println("New add stock stream...");
+          ;
+          addStockStreamObserver = client.getAsyncStub().addStock(new StreamObserver<StockMessage>() {
+
+            @Override
+            public void onNext(StockMessage value) {
+              // TODO Auto-generated method stub
+              System.out.println("Returned Value - " + value.getStockType() + " : " + value.getStockVolume());
+
+            }
+
+            @Override
+            public void onError(Throwable t) {
+              // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void onCompleted() {
+              // TODO Auto-generated method stub
+              System.out.println("Stream Ended");
+
+            }
+
+          });
+          newStreamButton.setEnabled(!newStreamButton.isEnabled());
+        }
+        // if the button is in close stream mode
+        else {
+          newStreamButton.setEnabled(!newStreamButton.isEnabled());
+          newStreamButton.setText("New Add Stock Stream");
+          addStockStreamObserver.onCompleted();
+          newStreamButton.setEnabled(!newStreamButton.isEnabled());
+
+        }
+
+      }
+
+    });
+
+    // add stock button listener
+    addButton.addActionListener(new ActionListener() {
+
+      @Override
+      public void actionPerformed(ActionEvent e) {
+
+        double stockVolume = Double.parseDouble(stockVolumeTextField.getText());
+        StockMessage stockMessage = StockMessage.newBuilder().setStockType((String) stockCJComboBox.getSelectedItem())
+            .setStockVolume(stockVolume).build();
+        addStockStreamObserver.onNext(stockMessage);
+
+      }
+
+    });
+
+    addStockPanel.add(newStreamButton);
     addStockPanel.add(stockCJComboBox);
+    stockCJComboBox.setEnabled(false);
+    addStockPanel.add(stockVolumeTextField);
+    stockVolumeTextField.setEnabled(false);
+    addStockPanel.add(addButton);
+    addButton.setEnabled(false);
 
     return addStockPanel;
   }
-	
+
 }
